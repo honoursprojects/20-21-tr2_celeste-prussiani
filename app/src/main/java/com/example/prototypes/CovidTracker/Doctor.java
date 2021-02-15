b7eac0dbd1d114ee8f13fac83daf887e12ed87c5
celestepru <celesteprussiani@outlook.it>
2021-02-15 23:27:21 +0000

    CovidTracker - Add number of cases to doctor library

--- app/src/main/java/com/example/prototypes/CovidTracker/Doctor.java
+++ app/src/main/java/com/example/prototypes/CovidTracker/Doctor.java
@@ -1,5 +1,7 @@
 package com.example.prototypes.CovidTracker;
 
+import com.example.prototypes.R;
+
 import java.util.HashMap;
 
 public class Doctor {
@@ -16,11 +18,12 @@ public class Doctor {
         String regulations = "The current COVID-19 regulations in the UK are: do not leave home except for essential purposes, adhere to social distancing" +
                 ", wear face masks where possible.";
         String friends = "You can meet with up to one person outside, but you have to adhere to social distancing. There are a few excpetions regarding affirmed couples," +
-                "children of divorced parents and people needing care.";
+                " children of divorced parents and people needing care.";
         String masks = "You must wear face masks indoors whenever possible. The correct face masks for your own health and safety and others' are surgical masks" +
                 "or FP2 masks. You can find them and other PPE here";
-        String gloves = "Gloves are recommended, but thorough hand washing is considered better. You can get some gloves from the store; for more information about PPE, go here";
-        String not_found = "Sorry, I'm afraid I don't understand the question. Could you try and rephrase?";
+        String gloves = "Gloves are recommended, but did you know thorough hand washing is much better? If you need them, you can get some gloves from the store; for more information about PPE, go here:";
+        String not_found = "Sorry, I'm afraid I' don't understand the question. Could you try and rephrase?";
+        String cases = "There are currently " + R.string.cases_no + " cases in the UK. The current R number is";
 
         library.put("regulations", regulations);
         library.put("rules", regulations);
@@ -33,6 +36,8 @@ public class Doctor {
         library.put("ppe", masks);
         library.put("error", not_found);
         library.put("welcome", welcome);
+        library.put("cases", cases);
+        library.put("deaths", cases);
     }
 
     public String greetUser() {
