b0cdfbe5c191cbe97bc8f30e4c6b2b5f36e60aad
celestepru <celesteprussiani@outlook.it>
2021-02-18 23:21:32 +0000

    CovidTracker - Updated buttons for home and chat view

--- app/src/main/java/com/example/prototypes/CovidTracker/CovidTrackerHome.java
+++ app/src/main/java/com/example/prototypes/CovidTracker/CovidTrackerHome.java
@@ -2,16 +2,23 @@ package com.example.prototypes.CovidTracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.prototypes.R;
 
 
 public class CovidTrackerHome extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid_tracker_home);
     }
 
+    public void openChat(View view) {
+        intent = new Intent(this, CovidTrackerChat.class);
+        startActivity(intent);
+    }
 }
