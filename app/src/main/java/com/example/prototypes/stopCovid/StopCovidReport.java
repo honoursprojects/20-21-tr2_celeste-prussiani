2d4a4d65a31531af610ab7544df3459e97b893d5
celestepru <celesteprussiani@outlook.it>
2021-03-11 15:26:39 +0000

    CovidTracker - report test page created; updated all button styles

---
+++ app/src/main/java/com/example/prototypes/stopCovid/StopCovidReport.java
@@ -0,0 +1,16 @@
+package com.example.prototypes.stopCovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prototypes.R;
+
+public class StopCovidReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_covid_report);
+    }
+}
