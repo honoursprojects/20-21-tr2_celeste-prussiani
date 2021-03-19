a49f2f67419835f06ab900565c81c3e31e701dc2
celestepru <celesteprussiani@outlook.it>
2021-03-19 16:21:30 +0000

    StopCovid - defined different notifications for different warnigns

--- app/src/main/java/com/example/prototypes/Application.java
+++ app/src/main/java/com/example/prototypes/Application.java
@@ -23,15 +23,23 @@ import java.util.ArrayList;
 
 public class Application extends android.app.Application {
 
-    public static final String WARNING_CHANNEL_NAME = "warningChannel";
-    public final int WARNING_CHANNEL_ID = 1;
-    private NotificationManagerCompat notificationManager;
-
-    BroadcastReceiver mReceiver;
-    public Boolean bluetooth = true;
-    public Boolean symptoms = true;
+    public static final String BLUETOOTH_CHANNEL_NAME = "warningChannel";
+    public final int BLUETOOTH_CHANNEL_ID = 1;
+    public static final String CONTACT_CHANNEL_NAME = "contactChannel";
+    public final int CONTACT_CHANNEL_ID = 2;
+    public static final String SYMPTOM_CHANNEL_NAME = "symptomChannel";
+    public final int SYMPTOM_CHANNEL_ID = 3;
     public final String BLUETOOTH_WARNING = "bluetooth";
     public final String SYMPTOMS_WARNING = "symptoms";
+    public final String CONTACT_WARNING = "contact";
+
+    private NotificationManagerCompat notificationManager;
+    BroadcastReceiver mReceiver;
+
+    public Boolean bluetooth = true;
+    public Boolean symptoms = true;
+    public Boolean contact = true;
+    public Boolean test = true;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -41,7 +49,13 @@ public class Application extends android.app.Application {
         notificationManager = NotificationManagerCompat.from(this);
         createNotificationChannels();
     }
+    /** Getters **/
+    public Boolean getBluetoothState() {return bluetooth;}
+    public Boolean getSymptomsState() {return symptoms;}
+    public Boolean getContactState() {return contact;}
+    public Boolean getTestState() {return test;}
 
+    /** Checkers **/
     public void checkSymptoms(ArrayList<String> newSymptoms) {
         if(newSymptoms.contains("Cough") && newSymptoms.contains("Fever")
                 && newSymptoms.contains("Breathing") && newSymptoms.contains("Taste")) {
@@ -53,12 +67,6 @@ public class Application extends android.app.Application {
         EventBus.getDefault().post(post);
         System.out.println("posted " + SYMPTOMS_WARNING);
     }
-
-
-    public Boolean getBluetoothState() {
-        return bluetooth;
-    }
-    public Boolean getSymptomsState() {return symptoms;}
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public Boolean checkBluetooth() {
@@ -90,14 +98,14 @@ public class Application extends android.app.Application {
                     if (btAdapter.getState() == BluetoothAdapter.STATE_OFF) {
                       //  displayToast("Bluetooth off");
                         bluetooth = false;
-                        showNotification();
+                        showNotification(BLUETOOTH_WARNING);
                         Warning post = new Warning(bluetooth, BLUETOOTH_WARNING);
                         EventBus.getDefault().post(post);
                     }
                     if(btAdapter.getState() == BluetoothAdapter.STATE_ON) {
                       //  displayToast("Bluetooth on");
                         bluetooth = true;
-                        cancelNotification(1);
+                        cancelNotification(BLUETOOTH_CHANNEL_ID);
                         Warning post = new Warning(bluetooth, BLUETOOTH_WARNING);
                         EventBus.getDefault().post(post);
                     }
@@ -109,34 +117,49 @@ public class Application extends android.app.Application {
         return bluetooth;
     }
 
-    public void displayToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+    /** Notifications **/
 
+    /**
+     * Create notification channels
+     */
     private void createNotificationChannels() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel warning = new NotificationChannel(
-                    WARNING_CHANNEL_NAME,
+                    BLUETOOTH_CHANNEL_NAME,
                     "WarningNotification",
                     NotificationManager.IMPORTANCE_HIGH
             );
             warning.setDescription("Warning no bluetooth on");
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(warning);
+
+            NotificationChannel contact = new NotificationChannel(
+                    CONTACT_CHANNEL_NAME,
+                    "ContactNotification",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            contact.setDescription("Warning dangerous contact");
+            manager.createNotificationChannel(contact);
         }
     }
 
-    public void showNotification() {
-        String message = "Please activate bluetooth to activate contact tracing";
+    /**
+     * Persistent warning notification
+     */
+    public void showNotification(String notificationType) {
+        String bluetoothMessage = "Please activate bluetooth to activate contact tracing";
+        String contactMessage = "You have been in contact with someone with Covid. You must self isolate now";
+        String symptomMessage = "You have reported dangerous symptoms. We advice self isolation";
+
         //Define which activity to open when tapping on notification
-        Intent activityIntent = new Intent(this, WarningMessage.class);
+        Intent activityIntent = new Intent(this, WarningMessage.class); //probably post a message to warning activity here
         PendingIntent contentIntent = PendingIntent.getActivity(this,
                 0, activityIntent, 0);
 
-        Notification warning = new NotificationCompat.Builder(this, WARNING_CHANNEL_NAME)
+        Notification warning = new NotificationCompat.Builder(this, BLUETOOTH_CHANNEL_NAME)
                 .setSmallIcon(R.drawable.ic_baseline_bluetooth_disabled_24)
                 .setContentTitle("Contact tracing disabled")
-                .setContentText(message)
+                .setContentText(bluetoothMessage)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setColor(Color.WHITE)
                 .setColor(getResources().getColor(R.color.colorDanger))
@@ -146,10 +169,49 @@ public class Application extends android.app.Application {
                 .setContentIntent(contentIntent)
                 .build();
 
+        Notification contact = new NotificationCompat.Builder(this, CONTACT_CHANNEL_NAME)
+                .setSmallIcon(R.drawable.ic_baseline_bluetooth_disabled_24)
+                .setContentTitle("Dangerous contact")
+                .setContentText(symptomMessage)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setColor(getResources().getColor(R.color.colorDanger))
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setContentIntent(contentIntent)
+                .build();
+
+        Notification symptoms = new NotificationCompat.Builder(this, SYMPTOM_CHANNEL_NAME)
+                .setSmallIcon(R.drawable.ic_baseline_bluetooth_disabled_24)
+                .setContentTitle("Dangerous symptoms")
+                .setContentText(contactMessage)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setColor(getResources().getColor(R.color.colorDanger))
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setContentIntent(contentIntent)
+                .build();
+
         //Display notification
-        notificationManager.notify(WARNING_CHANNEL_ID, warning);
+        switch(notificationType) {
+            case BLUETOOTH_WARNING:
+                notificationManager.notify(BLUETOOTH_CHANNEL_ID, warning);
+                break;
+            case CONTACT_WARNING:
+                notificationManager.notify(CONTACT_CHANNEL_ID, contact);
+                break;
+            case SYMPTOMS_WARNING:
+                notificationManager.notify(SYMPTOM_CHANNEL_ID, symptoms);
+                break;
+
+        }
     }
 
+    /**
+     * Cancel notifications
+     * @param id - channel id
+     */
     public void cancelNotification(int id) {
         notificationManager.cancel(id);
     }
