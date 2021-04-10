bf721eb53cdc4d57747fc1d5a31f5a3532fd951a
celestepru <celesteprussiani@outlook.it>
2021-04-10 16:17:13 +0100

    CovidTracker - added shopping cart functionality to add and remove items at will

--- app/src/main/java/com/example/prototypes/ShoppingCart.java
+++ app/src/main/java/com/example/prototypes/ShoppingCart.java
@@ -1,7 +1,10 @@
 package com.example.prototypes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -13,22 +16,28 @@ import java.util.Set;
 public class ShoppingCart extends AppCompatActivity {
 
     Cart cart;
+    LinearLayout msgBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
         SharedPreferences pref = getApplicationContext().getSharedPreferences("covidTracker_store", MODE_PRIVATE);
         cart = new Cart(pref);
+        msgBox = findViewById(R.id.msgContainer);
         display();
     }
 
     public void display() {
         Map<String, Integer> items = cart.retrieveItems();
         Set<String> itemNames = items.keySet();
-        for(String s : itemNames) {
-            int resID = getResources().getIdentifier(s, "id", getPackageName());
-            LinearLayout layout = findViewById(resID);
-            layout.setVisibility(View.VISIBLE);
+        if(!itemNames.isEmpty()) {
+            for(String s : itemNames) {
+                int resID = getResources().getIdentifier(s, "id", getPackageName());
+                LinearLayout layout = findViewById(resID);
+                layout.setVisibility(View.VISIBLE);
+                msgBox.setVisibility(View.GONE);
+            }
         }
     }
 
@@ -40,7 +49,7 @@ public class ShoppingCart extends AppCompatActivity {
         layout.setVisibility(View.GONE);
     }
 
-    public void clearAll(View view) {
+    public void clearItems() {
         LinearLayout ffp2 = findViewById(R.id.ffp2);
         LinearLayout ffp3 = findViewById(R.id.ffp3);
         LinearLayout surgical = findViewById(R.id.surgical);
@@ -53,9 +62,25 @@ public class ShoppingCart extends AppCompatActivity {
         visor.setVisibility(View.GONE);
         gloves.setVisibility(View.GONE);
         sanitiser.setVisibility(View.GONE);
-
+        msgBox.setVisibility(View.VISIBLE);
         cart.clearItems();
     }
 
+    public void clearAll(View view) {
+        clearItems();
+    }
+
+    public void buyItems(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("Thanks for your purchase")
+                .setMessage("The items you ordered will be shipped out to you today. Thanks for helping everyone keep safe and protected!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                       clearItems();
+                       dialog.dismiss();
+                    }
+                })
+                .show();
+        }
 
 }
